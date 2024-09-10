a2cb31757245e7fa8935c7ae6f295ce344568de1
MerkushevMA <s2dent59@gmail.com>
2024-09-10 21:38:18 +0500

    Update module_6_3_massinheritence.py

--- module_6_3_massinheritence.py
+++ module_6_3_massinheritence.py
@@ -1,28 +1,18 @@
 class Horse:
 
     def __init__(self):
-        # self.name = name
-        # self.sound = sound
-        # self.x_distance = x_distance
-        pass
-
-    sound = 'Frrr'
-    x_distance = 0
+        self.sound = 'Frrr'
+        self.x_distance = 0
 
     def run(self, dx):
         self.x_distance += dx
-        return self.x_distance
+
 
 class Eagle:
 
     def __init__(self):
-        # self.name = name
-        # self.sound = sound
-        # self.y_distance = y_distance
-        pass
-
-    sound = 'I train, eat, sleep, and repeat'
-    y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
+        self.y_distance = 0
 
     def fly(self, dy):
         self.y_distance += dy
@@ -31,8 +21,8 @@ class Eagle:
 class Pegasus(Horse, Eagle):
 
     def __init__(self):
-        Eagle.__init__(self)
         Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         self.run(dx)
@@ -47,7 +37,6 @@ class Pegasus(Horse, Eagle):
 
 p1 = Pegasus()
 
-
 print(p1.get_pos())
 p1.move(10, 15)
 print(p1.get_pos())
@@ -55,7 +44,3 @@ p1.move(-5, 20)
 print(p1.get_pos())
 
 p1.voice()
-
-print(Pegasus.mro())
-print(Horse.mro())
-print(Eagle.mro())
